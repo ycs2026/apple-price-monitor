fec6c5bf58f7ccba1bb48d5ce49103a774279a23
ycs2026 <yangchunsheng2013@hotmail.com>
2026-07-24 12:46:55 +0800

    Add product list with details to config.js
    Added a list of product objects with details including id, name, jd, and apple properties.

--- src/config.js
+++ src/config.js
@@ -1,13 +1,75 @@
-export const CONFIG = {
+export const PRODUCTS = [
 
-    telegram: {
+  {
 
-        botToken: "",
+    id: "iphone17pro128",
 
-        chatId: ""
+    name: "iPhone 17 Pro 128GB",
 
-    },
+    jd: "",
 
-    products: []
+    apple: ""
 
-}
+  },
+
+  {
+
+    id: "iphone17pro256",
+
+    name: "iPhone 17 Pro 256GB",
+
+    jd: "",
+
+    apple: ""
+
+  },
+
+  {
+
+    id: "iphone17promax256",
+
+    name: "iPhone 17 Pro Max 256GB",
+
+    jd: "",
+
+    apple: ""
+
+  },
+
+  {
+
+    id: "iphone17air256",
+
+    name: "iPhone Air 256GB",
+
+    jd: "",
+
+    apple: ""
+
+  },
+
+  {
+
+    id: "iphone17256",
+
+    name: "iPhone 17 256GB",
+
+    jd: "",
+
+    apple: ""
+
+  },
+
+  {
+
+    id: "iphone17e128",
+
+    name: "iPhone 17e 128GB",
+
+    jd: "",
+
+    apple: ""
+
+  }
+
+];
