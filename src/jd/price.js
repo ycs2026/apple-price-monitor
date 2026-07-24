f35536d795b5bb41b1ece8698c5eeb362e51abbc
ycs2026 <yangchunsheng2013@hotmail.com>
2026-07-24 13:25:29 +0800

    Update price.js

--- src/jd/price.js
+++ src/jd/price.js
@@ -1,23 +1,43 @@
-export async function getJDPrice(url) {
+const IDS = [
 
-    const res = await fetch(url, {
+  "100278222276", // iPhone 17 256GB
 
-        redirect: "follow",
+  "100209268163", // iPhone 17 Pro 256GB
 
-        headers: {
+  "100209268193", // iPhone 17 Pro Max 256GB
 
-            "User-Agent": "Mozilla/5.0"
+  "100142621616", // iPhone 16 256GB
 
-        }
+  "100142621626", // iPhone 16 Pro 128GB
 
-    });
+  "100142621610", // iPhone 16 Plus 256GB
 
-    return {
+  "100278221440"  // iPhone Air 256GB
 
-        finalUrl: res.url,
+];
 
-        status: res.status
+export async function getPrices() {
 
-    };
+  const url =
+
+    "https://p.3.cn/prices/mgets?type=1&skuIds=J_" +
+
+    IDS.join(",J_");
+
+  const res = await fetch(url, {
+
+    headers: {
+
+      "User-Agent": "Mozilla/5.0"
+
+    }
+
+  });
+
+  const text = await res.text();
+
+  console.log(text);
+
+  return text;
 
 }
